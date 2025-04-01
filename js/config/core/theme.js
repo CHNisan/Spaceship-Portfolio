09ed2f9a2ad2434040816ba156235bbe46456c44
CNisan <christopherhenrynisan@gmail.com>
2025-04-01 13:10:39 +0100

    Changed to pastle colour scheme

--- js/config/core/theme.js
+++ js/config/core/theme.js
@@ -14,10 +14,10 @@ const themeConfig = {
     
     COLORS: {
         RED: 0xF76565,
-        YELLOW: 0xFFAA00,
-        GREEN: 0x00FF00,
-        BLUE: 0x00FFFF,
-        PINK: 0xFF00FF
+        YELLOW: 0xF9EB66,
+        GREEN: 0x8AF984,
+        BLUE: 0x4AA8E2,
+        PINK: 0xFFA8E2
     }
 };
 
